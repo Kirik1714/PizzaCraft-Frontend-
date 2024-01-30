7d429e6e08b6583b85d7eb8da11d062c2ee13cc8
Krill Shatsila <kirill737shatilo@gmail.com>
2024-01-31 01:09:06 +0300

    Add pagination and Skeleton for pizza

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,93 +1,119 @@
 <script setup>
-import { onMounted,ref ,watch, watchEffect} from 'vue';
+import Paginator from 'primevue/paginator';
+import PizzaSkeleton from './PizzaSkeleton.vue';
+
+import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
 import Filter from './Filter.vue';
 import Pizza from './Pizza.vue'
 
-const pizzaz=ref([])
+const pizzaz = ref([])
 
-
+const page = ref(0)
+const totalCountPizzas = ref()
+const selectedSorted = ref();
+const isLoadingPizza = ref(true);
 const filters = ref([
     { id: 1, name: 'Мясная' },
-    { id: 2, name: 'Морская' },
-    { id: 3, name: 'Острая' },
-    { id: 4, name: 'Белая'},
-    { id: 5, name: 'Фруктовая'},
-    { id: 6, name: 'По сезону'},
-    
+    { id: 2, name: 'Вегетарианская' },
+    { id: 3, name: 'Морская' },
+    { id: 4, name: 'Острая' },
+    { id: 5, name: 'Белая' },
+    { id: 6, name: 'По сезону' },
+    { id: 7, name: 'Сладкая' },
+    { id: 8, name: 'Все' },
+
+
 ]);
-const selectedFilter = ref([filters.value[0].id]);
+const selectedFilter = ref([filters.value[filters.value.length - 1].id]);
 const changeFilter = (id) => {
-  selectedFilter.value = [id];
-  
+    selectedFilter.value = [id];
+
 }
 
-const selectedSorted = ref();
+
 const sortItems = ref([
     { id: 1, name: 'Возрастанию', code: 'asc' },
-    {  id: 2,  name: 'Убыванию', code: 'desc' },
+    { id: 2, name: 'Убыванию', code: 'desc' },
 
 ]);
-const installSortedItem=(obj)=>{
-  selectedSorted.value=obj
+const installSortedItem = (obj) => {
+    selectedSorted.value = obj
 }
 
 
 
 
-const getPizzas = async() => {
+const getPizzas = async () => {
     try {
-        const {data}= await axios.post('http://localhost:8000/api/pizza',{
-            'categories':selectedFilter.value,
-            'sorted':selectedSorted.value?.code
+        const { data } = await axios.post('http://localhost:8000/api/pizza', {
+            'categories': selectedFilter.value,
+            'sorted': selectedSorted.value?.code,
+            'page': page.value + 1
         })
-        pizzaz.value=data
+        pizzaz.value = data.data
+        totalCountPizzas.value = data.total;
+        isLoadingPizza.value = false;
     } catch (error) {
         console.log('Не удалочь получить пиццы', error)
     }
 }
 
 onMounted(() => {
-   
+    getPizzas();
+
+})
+
+watch([selectedFilter, selectedSorted, page], () => {
     getPizzas()
-})
-watchEffect( ()=>{
- 
-
-      getPizzas()
 
 })
+
 </script>
 
 <template>
     <div class="container">
         <div class="container_filters">
 
-            <Filter :filters="filters" :selectedFilter="selectedFilter" @installSortedItem="installSortedItem" :sortItems="sortItems" @changeFilter="changeFilter" />
+            <Filter :filters="filters" :selectedFilter="selectedFilter" @installSortedItem="installSortedItem"
+                :sortItems="sortItems" @changeFilter="changeFilter" />
         </div>
+
         <div class="container_main">
             <div class="container_main_title">Все пиццы</div>
             <div class="container_main_block">
-                <Pizza v-for="pizza in pizzaz" :key="pizza.id" :pizza="pizza"/>
-            
+                <template v-if="!isLoadingPizza">
+                    <Pizza v-for="pizza in pizzaz" :key="pizza.id" :pizza="pizza" />
+                </template>
+                <template v-else>
+                   <PizzaSkeleton v-for="i in 4" :key="i"/>
+                </template>
             </div>
         </div>
-      
-        
+
+        <div class="footer" v-if="!isLoadingPizza">
+            <Paginator :rows="4" :totalRecords="totalCountPizzas" @page="page = $event.page">
+
+            </Paginator>
+
+        </div>
+
+
+
+
     </div>
-   
 </template>
 
 <style scoped  lang="scss">
-
-.container{
+.container {
     height: 100%;
     padding: 10px;
-    &_main{
-        font-size: 32px;
-        margin-top:32px ;
 
-        &_block{
+    &_main {
+        font-size: 32px;
+        margin-top: 32px;
+
+        &_block {
             display: grid;
             grid-template-columns: 1fr 1fr 1fr 1fr;
             gap: 32px;
---
+++ src/components/PizzaSkeleton.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Skeleton from 'primevue/skeleton';
+</script>
+
+<template>
+    <div class="container">
+
+        <Skeleton shape="circle" size="14.4rem" class="container_img"></Skeleton>
+        <Skeleton class="container_title"></Skeleton>
+
+
+        <Skeleton width="100%" height="85px"></Skeleton>
+
+        <div class="container_pizza_price_add">
+            <Skeleton width="5rem" height="2.2rem" ></Skeleton>
+            <Skeleton width="8rem" height="2.2rem"></Skeleton>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.container {
+    width: 250px;
+    height: 250px;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 35px;
+
+    &_title {
+        height: 40px;
+        font-size: 20px;
+        margin-top: 11px;
+        margin-bottom: 10px;
+
+    }
+    &_pizza_price_add {
+        display: flex;
+        font-size: 20px;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 15px;
+        width: 100%;
+    }
+}
+</style>
